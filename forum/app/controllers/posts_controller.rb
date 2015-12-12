458392d01dc34530de2b82aaebed3e1cb2bc35ac
darkhelios <tomas.ibarrab@gmail.com>
2015-12-11 17:16:05 -0700

    Agregado tiempo de publicación y lista de posts

--- forum/app/controllers/posts_controller.rb
+++ forum/app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
 
+	before_action :find_post, only: [:show, :update, :edit, :destroy]
+
 	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+
+	def show
 	end
 
 	def new
@@ -17,7 +23,28 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update 
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
 	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
 	def post_params
 		params.require(:post).permit(:title, :content)
 	end
